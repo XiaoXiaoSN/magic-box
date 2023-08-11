0a21689f9e57efe2582d869c0e2aabd2135443d5
XiaoXiaoSN <xiao.xiao@tenoz.tw>
2023-08-12 03:10:49 +0800

    style: fix styling issues

--- src/pages/MagicBox/MagicBoxPage.tsx
+++ src/pages/MagicBox/MagicBoxPage.tsx
@@ -2,12 +2,9 @@ import MagicBox from '@components/MagicBox';
 import {
   Container, Grid, TextField,
 } from '@mui/material';
-// import { useTheme } from '@mui/material/styles';
 import React, { useEffect, useState } from 'react';
 
 const MagicBoxPage = () => {
-  // const theme = useTheme();
-
   const [userInput, setUserInput] = useState('');
   const [magicIn, setMagicIn] = useState('');
 
@@ -20,10 +17,9 @@ const MagicBoxPage = () => {
   // https://mui.com/material-ui/api/container/#props
   const containerMaxWidth = '95vw' as never;
 
-  // style="height: 50px; padding-right: 10px"
   return (
     <Container maxWidth={containerMaxWidth}>
-      <Grid container spacing={2} sx={{ marginTop: '30px', padding: '30px' }}>
+      <Grid container spacing={1.5} sx={{ marginTop: '20px', padding: '30px' }}>
 
         {/* Magic Box Input */}
         <Grid item xs={12} sm={12} md={6}>
@@ -41,15 +37,22 @@ const MagicBoxPage = () => {
         </Grid>
 
         {/* Magic Box Output */}
-        <Grid
-          container
-          item
-          xs={12}
-          sm={12}
-          md={6}
-          sx={{ background: '#f5f5f5', overflow: 'scroll', maxHeight: 'calc(100vh - 100px)' }}
-        >
-          <MagicBox input={magicIn} />
+        <Grid item xs={12} sm={12} md={6}>
+          <Grid
+            container
+            justifyContent="center"
+            alignItems="center"
+            padding=".75rem"
+            sx={{
+              background: '#f5f5f5',
+              overflow: 'scroll',
+              '&::-webkit-scrollbar': { display: 'none' },
+              scrollbarWidth: 'none', // Firefox
+              maxHeight: 'calc(100dvh - 150px)',
+            }}
+          >
+            <MagicBox input={magicIn} />
+          </Grid>
         </Grid>
 
       </Grid>
