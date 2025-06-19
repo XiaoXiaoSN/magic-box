22e62482e5e0bfd96d2becbe2fb546bf10fdeea2
XiaoXiaoSN <xiao.xiao@tenoz.tw>
2025-06-20 00:20:43 +0800

    feat: add sentry

--- src/global/env.ts
+++ src/global/env.ts
@@ -2,6 +2,8 @@
 
 const env = {
   TOOLBOX: import.meta.env.VITE_TOOLBOX ?? 'https://tool.10oz.tw',
+
+  SENTRY_DSN: 'https://b695bfd011d8dc591cde6ca368f362e7@o510009.ingest.us.sentry.io/4509526138224640',
 };
 
 export default env;
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,20 @@
 import './index.css';
 import './firebaseConfig';
 
+import env from '@global/env';
 import CssBaseline from '@mui/material/CssBaseline';
+import * as Sentry from '@sentry/react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App';
+
+Sentry.init({
+  dsn: env.SENTRY_DSN,
+  // Setting this option to true will send default PII data to Sentry.
+  // For example, automatic IP address collection on events
+  sendDefaultPii: true,
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
