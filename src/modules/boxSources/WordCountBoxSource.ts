4936a7fc71654b95c22e923e85e698d71986f5a4
XiaoXiaoSN <xiao.xiao@tenoz.tw>
2025-06-21 13:38:54 +0800

    feat: add WordCountBox

---
+++ src/modules/boxSources/WordCountBoxSource.ts
@@ -0,0 +1,71 @@
+import { KeyValueBoxTemplate } from '@components/BoxTemplate';
+import { isString, trim } from '@functions/helper';
+import { BoxBuilder } from '@modules/Box';
+
+import type { Box } from '@modules/Box';
+
+const Priority = 0;
+
+interface Match {
+  text: string,
+}
+
+function countLines(str: string) {
+  if (str === '') {
+    return 0;
+  }
+  return (str.match(/\n/g) ?? []).length + 1;
+}
+
+function countWords(str: string) {
+  if (str.trim() === '') {
+    return 0;
+  }
+  return str.trim().split(/\s+/).length;
+}
+
+export const WordCountBoxSource = {
+  checkMatch(input: string): Match | undefined {
+    if (!isString(input)) {
+      return undefined;
+    }
+    const regularInput = trim(input);
+
+    if (regularInput.length <= 0) {
+      return undefined;
+    }
+
+    return { text: regularInput };
+  },
+
+  async generateBoxes(input: string): Promise<Box[]> {
+    const match = this.checkMatch(input);
+    if (!match) {
+      return [];
+    }
+
+    const lines = countLines(match.text);
+    const words = countWords(match.text);
+    const characters = match.text.length;
+
+    const content = `lines: ${lines}
+words: ${words}
+characters: ${characters}`;
+
+    const output: Record<string, string> = {
+      lines: lines.toString(),
+      words: words.toString(),
+      characters: characters.toString(),
+    };
+
+    return [
+      new BoxBuilder('Word Count', content)
+        .setTemplate(KeyValueBoxTemplate)
+        .setOptions(output)
+        .setPriority(Priority)
+        .build(),
+    ];
+  },
+};
+
+export default WordCountBoxSource;
