8766af8c8372ca1c03c359fc7351589cc3175d97
XiaoXiaoSN <xiao.xiao@tenoz.tw>
2026-06-24 14:02:23 +0800

    feat(Bifid): add Bifid BoxSource

--- src/modules/boxSources/index.ts
+++ src/modules/boxSources/index.ts
@@ -5,6 +5,7 @@ import {
   Base64DecodeBoxSource,
   Base64EncodeBoxSource,
 } from './Base64BoxSource';
+import BifidBoxSource from './BifidBoxSource';
 import BinaryTextBoxSource from './BinaryTextBoxSource';
 import BmiBoxSource from './BmiBoxSource';
 import ColorBoxSource from './ColorBoxSource';
@@ -74,6 +75,7 @@ export const boxSources: BoxSource[] = [
   WordWrapBoxSource,
   A1Z26BoxSource,
   AsciiTableBoxSource,
+  BifidBoxSource,
   BinaryTextBoxSource,
   BmiBoxSource,
   FrequencyBoxSource,
