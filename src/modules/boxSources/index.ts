d584c2d13fb2bfe6995a50aa6cf7abe665c5e90f
XiaoXiaoSN <xiao.xiao@tenoz.tw>
2026-06-24 14:22:34 +0800

    feat(UnicodeEscape): add UnicodeEscape BoxSource

--- src/modules/boxSources/index.ts
+++ src/modules/boxSources/index.ts
@@ -61,6 +61,7 @@ import TimeFormatBoxSource from './TimeFormatBoxSource';
 import TimestampBoxSource from './TimestampBoxSource';
 import TwosComplementBoxSource from './TwosComplementBoxSource';
 import UlidBoxSource from './UlidBoxSource';
+import UnicodeEscapeBoxSource from './UnicodeEscapeBoxSource';
 import UnicodeNormalizeBoxSource from './UnicodeNormalizeBoxSource';
 import UnitConverterBoxSource from './UnitConverterBoxSource';
 import URLDecodeBoxSource from './URLDecodeBoxSource';
@@ -148,4 +149,5 @@ export const boxSources: BoxSource[] = [
   MacAddressBoxSource,
   MorseCodeBoxSource,
   PortLookupBoxSource,
+  UnicodeEscapeBoxSource,
 ];
