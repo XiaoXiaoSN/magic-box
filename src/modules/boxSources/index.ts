fcdc759872bebb59dc1c39d72af8ee34853f6d15
XiaoXiaoSN <xiao.xiao@tenoz.tw>
2026-06-24 14:01:38 +0800

    feat(Base58): add Base58 BoxSource

--- src/modules/boxSources/index.ts
+++ src/modules/boxSources/index.ts
@@ -5,6 +5,7 @@ import {
   Base64DecodeBoxSource,
   Base64EncodeBoxSource,
 } from './Base64BoxSource';
+import Base58BoxSource from './Base58BoxSource';
 import BinaryTextBoxSource from './BinaryTextBoxSource';
 import BmiBoxSource from './BmiBoxSource';
 import ColorBoxSource from './ColorBoxSource';
@@ -83,6 +84,7 @@ export const boxSources: BoxSource[] = [
   WordWrapBoxSource,
   A1Z26BoxSource,
   AsciiTableBoxSource,
+  Base58BoxSource,
   BinaryTextBoxSource,
   BmiBoxSource,
   FractionBoxSource,
